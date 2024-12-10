c77f83937509291296f0ad131816e1aef42f4a06
srilusiana <srilusiana46@gmail.com>
2024-12-10 22:18:09 +0700

    Tugas Praktikum Pekan 6

--- app/Http/Controllers/StudentController.php
+++ app/Http/Controllers/StudentController.php
@@ -1,8 +1,9 @@
 <?php
 
 namespace App\Http\Controllers;
-
+use App\Models\Student;
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Validator;
 
 class StudentController extends Controller
 {
